b19bf21945416fa058f23fc2959d00cb50861124
Magnus Manske <magnusmanske@googlemail.com>
2019-07-09 15:28:34 +0100

    refactor app_state

---
+++ src/app_state.rs
@@ -0,0 +1,36 @@
+extern crate rocket;
+
+use mysql as my;
+use serde_json::Value;
+use std::sync::Arc;
+
+pub struct AppState {
+    pub db: Arc<my::Pool>,
+    pub config: Value,
+}
+
+impl AppState {
+    pub fn new_from_config(config: &Value) -> Self {
+        Self {
+            db: Arc::new(AppState::db_pool_from_config(config)),
+            config: config.to_owned(),
+        }
+    }
+
+    fn db_pool_from_config(config: &Value) -> my::Pool {
+        let mut builder = my::OptsBuilder::new();
+        //println!("{}", &self.params);
+        builder
+            .ip_or_hostname(config["host"].as_str())
+            .db_name(config["schema"].as_str())
+            .user(config["user"].as_str())
+            .pass(config["password"].as_str());
+        builder.tcp_port(config["db_port"].as_u64().unwrap_or(3306) as u16);
+
+        // Min 1, max 7 connections
+        match my::Pool::new_manual(1, 7, builder) {
+            Ok(pool) => pool,
+            Err(e) => panic!("Could not initialize DB connection pool: {}", &e),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,20 @@
 #[macro_use]
 extern crate rocket;
 
+pub mod app_state;
 pub mod form_parameters;
 
+use app_state::AppState;
 use form_parameters::FormParameters;
-use mysql as my;
+//use mysql as my;
 use rocket::config::{Config, Environment};
 use rocket::request::LenientForm;
+use rocket::State;
 use rocket_contrib::serve::StaticFiles;
 use serde_json::Value;
 use std::env;
 use std::fs::File;
-use std::sync::Arc;
-
-pub struct AppState {
-    pub db: Arc<my::Pool>,
-    pub config: Value,
-}
-
-impl AppState {
-    pub fn new_from_config(config: &Value) -> Self {
-        Self {
-            db: Arc::new(AppState::db_pool_from_config(config)),
-            config: config.to_owned(),
-        }
-    }
-
-    fn db_pool_from_config(config: &Value) -> my::Pool {
-        let mut builder = my::OptsBuilder::new();
-        //println!("{}", &self.params);
-        builder
-            .ip_or_hostname(config["host"].as_str())
-            .db_name(config["schema"].as_str())
-            .user(config["user"].as_str())
-            .pass(config["password"].as_str());
-        builder.tcp_port(config["db_port"].as_u64().unwrap_or(3306) as u16);
-
-        // Min 1, max 7 connections
-        match my::Pool::new_manual(1, 7, builder) {
-            Ok(pool) => pool,
-            Err(e) => panic!("Could not initialize DB connection pool: {}", &e),
-        }
-    }
-}
-
-use rocket::State;
+//use std::sync::Arc;
 
 fn process_form(form_parameters: LenientForm<FormParameters>, _state: State<AppState>) -> String {
     format!(
