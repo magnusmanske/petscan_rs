ed428737b1325ca807eb9d74caba65796872ac60
Magnus Manske <magnusmanske@googlemail.com>
2019-07-10 09:25:15 +0100

    misc

--- src/platform.rs
+++ src/platform.rs
@@ -3,44 +3,38 @@ use crate::form_parameters::FormParameters;
 use rocket::request::State;
 use std::sync::Arc;
 
-//#[derive(Copy, Clone)]
+#[derive(Clone)]
 pub struct Platform {
-    pub form_parameters: FormParameters,
-    pub state: AppState,
+    pub form_parameters: Arc<FormParameters>,
+    pub state: Arc<AppState>,
 }
 
-pub trait DataSource {
-    //fn new_from_platform(platform: &Platform) -> Self;
-}
+pub trait DataSource {}
 
-#[derive(Copy, Clone)]
-pub struct SourceDatabase {
-    platform: Arc<&Platform>,
-}
+#[derive(Clone)]
+pub struct SourceDatabase {}
 
 impl DataSource for SourceDatabase {}
 
 impl SourceDatabase {
-    pub fn new_from_platform(platform: Arc<&Platform>) -> Self {
-        Self { platform }
+    pub fn new_from_platform() -> Self {
+        Self {}
     }
 }
 
 impl Platform {
     pub fn new_from_parameters(form_parameters: &FormParameters, state: State<AppState>) -> Self {
         Self {
-            form_parameters: (*form_parameters).clone(),
-            state: state.inner().clone(),
+            form_parameters: Arc::new((*form_parameters).clone()),
+            state: Arc::new(state.inner().clone()),
         }
     }
 
     pub fn run(&mut self) {
         // TODO legacy parameters
 
-        let me = Arc::new(&self);
-
-        let _x = SourceDatabase::new_from_platform(me.clone());
-        //let mut candidate_sources: Vec<dyn DataSource> = vec![];
-        //candidate_sources.push(SourceDatabase::new_from_platform(&self));
+        let x = SourceDatabase::new_from_platform();
+        let mut candidate_sources: Vec<dyn DataSource> = vec![];
+        candidate_sources.push(x);
     }
 }
