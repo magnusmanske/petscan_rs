114c462792c8863a77915ebac018ea2b661e5328
Magnus Manske <magnusmanske@googlemail.com>
2019-07-12 16:43:12 +0100

    misc

--- src/datasource_database.rs
+++ src/datasource_database.rs
@@ -2,6 +2,9 @@ use crate::datasource::DataSource;
 use crate::datasource::SQLtuple;
 use crate::pagelist::*;
 use crate::platform::Platform;
+use chrono::prelude::*;
+use chrono::Duration;
+use core::ops::Sub;
 use mediawiki::api::{Api, NamespaceID};
 use mediawiki::title::Title;
 use mysql as my;
@@ -22,7 +25,14 @@ pub struct SourceDatabaseCatDepth {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct SourceDatabase {}
+pub struct SourceDatabase {
+    cat_pos: Vec<Vec<String>>,
+    cat_neg: Vec<Vec<String>>,
+    has_pos_cats: bool,
+    has_neg_cats: bool,
+    has_pos_templates: bool,
+    has_pos_linked_from: bool,
+}
 
 impl DataSource for SourceDatabase {
     fn name(&self) -> String {
@@ -41,7 +51,14 @@ impl DataSource for SourceDatabase {
 
 impl SourceDatabase {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            cat_pos: vec![],
+            cat_neg: vec![],
+            has_pos_cats: false,
+            has_neg_cats: false,
+            has_pos_templates: false,
+            has_pos_linked_from: false,
+        }
     }
 
     fn go_depth(
@@ -213,9 +230,175 @@ impl SourceDatabase {
         ret
     }
 
-    /*
-    bool TSourceDatabase::getPages () {
-    bool TSourceDatabase::getPagesforPrimary ( TWikidataDB &db , string primary , string sql , string sql_before_after , vector <TPage> &pages_sublist , bool is_before_after_done ) {
+    fn parse_category_depth(
+        &self,
+        cats: &Vec<String>,
+        default_depth: u16,
+    ) -> Vec<SourceDatabaseCatDepth> {
+        cats.iter()
+            .filter_map(|c| {
+                let mut parts = c.split("|");
+                let name = match parts.next() {
+                    Some(n) => n.to_string(),
+                    None => return None,
+                };
+                let depth = match parts.next() {
+                    Some(depth) => depth.parse::<u16>().ok()?,
+                    None => return None,
+                };
+                Some(SourceDatabaseCatDepth {
+                    name: name,
+                    depth: depth,
+                })
+            })
+            .collect()
+    }
 
-    */
+    pub fn get_pages(
+        &mut self,
+        platform: &Platform,
+        primary_pagelist: Option<&PageList>,
+    ) -> Option<PageList> {
+        let wiki = match primary_pagelist {
+            Some(pl) => pl.wiki.clone(),
+            None => platform.get_param("wiki"),
+        };
+
+        // Paranoia
+        if wiki.is_none() || wiki == Some("wiki".to_string()) {
+            return None;
+        }
+        let wiki = wiki.unwrap();
+
+        let db_user_pass = platform.state.get_db_mutex().lock().unwrap(); // Force DB connection placeholder
+        let mut ret = PageList::new_from_wiki(&wiki);
+        let mut conn = platform
+            .state
+            .get_wiki_db_connection(&db_user_pass, &wiki)?;
+
+        let depth = platform
+            .get_param("depth")
+            .unwrap_or("0".to_string())
+            .parse::<u16>()
+            .ok()?;
+        let cat_pos =
+            self.parse_category_depth(&platform.get_param_as_vec("categories", "\n"), depth);
+        let cat_neg = self.parse_category_depth(&platform.get_param_as_vec("negcats", "\n"), depth);
+        self.cat_pos = self.parse_category_list(&mut conn, &cat_pos);
+        self.cat_neg = self.parse_category_list(&mut conn, &cat_neg);
+
+        let templates_yes = platform.get_param_as_vec("templates_yes", "\n");
+        let templates_any = platform.get_param_as_vec("templates_any", "\n");
+        //let templates_no = platform.get_param_as_vec("templates_no", "\n");
+
+        let linked_from_all = platform.get_param_as_vec("outlinks_yes", "\n");
+        let linked_from_any = platform.get_param_as_vec("outlinks_any", "\n");
+        let _linked_from_none = platform.get_param_as_vec("outlinks_no", "\n");
+
+        let links_to_all = platform.get_param_as_vec("links_to_all", "\n");
+        let links_to_any = platform.get_param_as_vec("links_to_any", "\n");
+        let _links_to_none = platform.get_param_as_vec("links_to_no", "\n");
+
+        self.has_pos_templates = !templates_yes.is_empty() || !templates_any.is_empty();
+        self.has_pos_linked_from = !linked_from_all.is_empty()
+            || !linked_from_any.is_empty()
+            || !links_to_all.is_empty()
+            || !links_to_any.is_empty();
+
+        let mut primary: String;
+        if !self.cat_pos.is_empty() {
+            primary = "categories".to_string();
+        } else if self.has_pos_templates {
+            primary = "templates".to_string();
+        } else if self.has_pos_linked_from {
+            primary = "links_from".to_string();
+        } else if primary_pagelist.is_some() {
+            primary = "pagelist".to_string();
+        } else if platform.get_param("page_wikidata_item") == Some("without".to_string()) {
+            primary = "no_wikidata".to_string();
+        } else {
+            return None;
+        }
+        println!("PRIMARY: {}", &primary);
+
+        let lc = if platform.has_param("minlinks") || platform.has_param("maxlinks") {
+            ",(SELECT count(*) FROM pagelinks WHERE pl_from=p.page_id) AS link_count"
+        } else {
+            ""
+        };
+
+        let mut sql_before_after = Platform::sql_tuple();
+        let mut before: String = "".to_string();
+        let mut after: String = "".to_string();
+        let mut before_after: String = "".to_string();
+        let mut is_before_after_done: bool = false;
+        match platform.get_param("max_age") {
+            Some(max_age) => {
+                let utc: DateTime<Utc> = Utc::now();
+                let utc = utc.sub(Duration::hours(max_age.parse::<i64>().unwrap_or(0)));
+                before = "".to_string();
+                after = utc.format("%Y%m%d%H%M%S").to_string();
+            }
+            None => {}
+        }
+
+        if !before.is_empty() || after.is_empty() {
+            is_before_after_done = true;
+        } else {
+            sql_before_after.0 = " INNER JOIN (revision r) on r.rev_page=p.page_id".to_string();
+            if platform.has_param("only_new_since") {
+                sql_before_after.0 += " AND r.rev_parent_id=0";
+            }
+            if !before.is_empty() {
+                sql_before_after.0 += " AND rev_timestamp<=?";
+                sql_before_after.1.push(before);
+            }
+            if !after.is_empty() {
+                sql_before_after.0 += " AND rev_timestamp>=?";
+                sql_before_after.1.push(after);
+            }
+            sql_before_after.0 += " ";
+        }
+
+        let mut sql = Platform::sql_tuple();
+
+        match primary.as_str() {
+            "categories" => {}
+            "no_wikidata" => {}
+            "templates" | "links_from" => {}
+            "pagelist" => {}
+            other => {
+                println!("SourceDatabase::get_pages: other primary '{}'", &other);
+                return None;
+            }
+        }
+
+        //if ( !getPagesforPrimary ( db , primary , sql , sql_before_after , pages , is_before_after_done ) ) return false ;
+
+        if self.get_pages_for_primary(
+            &mut conn,
+            &primary,
+            &mut sql,
+            &sql_before_after,
+            &ret,
+            is_before_after_done,
+        ) {
+            //data_loaded = true ;
+            Some(ret)
+        } else {
+            None
+        }
+    }
+
+    fn get_pages_for_primary(
+        &self,
+        conn: &mut my::Conn,
+        primary: &String,
+        sql: &mut SQLtuple,
+        sql_before_after: &SQLtuple,
+        pages_sublist: &PageList,
+        is_before_after_done: bool,
+    ) -> bool {
+        false
+    }
 }
