b5d99140a651264cffc1defde79fb72e1428f466
Magnus Manske <magnusmanske@googlemail.com>
2019-07-23 13:53:44 +0100

    misc

---
+++ update_live_site.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Get latest code
+git pull
+
+# Update
+cargo update
+
+# Build new server binary
+cargo build --release
+
+# Get restart code from config file
+code=`jq -r '.["restart-code"]' config.json`
+
+# Build restart URL
+url="http://127.0.0.1/?restart=$code"
+
+# Restart server
+curl -s -o /dev/null $url
+echo "Server restarted!"
