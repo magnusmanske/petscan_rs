12601b03e2d522e51bfb04d553c9fc095e15a411
Magnus Manske <magnusmanske@googlemail.com>
2019-07-24 18:53:35 +0100

    service based server

--- update_live_site.sh
+++ update_live_site.sh
@@ -17,5 +17,6 @@ url="http://127.0.0.1/?restart=$code"
 # Restart server
 curl -s -o /dev/null $url
 sleep 1
-service ./target/release/petscan_rs restart
+#screen -r
+#/usr/sbin/service ./target/release/petscan_rs restart
 
