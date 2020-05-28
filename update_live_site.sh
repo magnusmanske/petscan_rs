2c71069016703e3996a8d8fde982fd6bd7a9c9c1
Magnus Manske <magnusmanske@googlemail.com>
2020-05-28 08:47:42 +0000

    watcher script

--- update_live_site.sh
+++ update_live_site.sh
@@ -9,14 +9,14 @@ cargo update
 cargo build --release
 
 # Get restart code from config file
-code=`jq -r '.["restart-code"]' config.json`
+#code=`jq -r '.["restart-code"]' config.json`
 
 # Build restart URL
-url="http://127.0.0.1/?restart=$code"
+#url="http://127.0.0.1/?restart=$code"
 
 # Restart server
-curl -s -o /dev/null $url
+#curl -s -o /dev/null $url
 #sleep 1
-screen -r pts-0.petscan4
+#screen -r pts-0.petscan4
 #/usr/sbin/service ./target/release/petscan_rs restart
-
+sudo killall petscan_rs
