5cefb43fcc00c060249b1910f63637de24102634
Magnus Manske <magnusmanske@googlemail.com>
2024-01-19 11:32:25 +0000

    update to hyper v1

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,29 @@ pub mod platform;
 pub mod render;
 pub mod wdfist;
 
-use tokio::fs::File as TokioFile;
-use tokio_util::codec::{BytesCodec, FramedRead};
-use url::form_urlencoded;
+use std::convert::Infallible;
+use std::env;
+use std::net::SocketAddr;
+
+use http_body_util::{Full, BodyExt};
+use hyper::body::{Bytes, Body};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, header, StatusCode, Method};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
 use crate::form_parameters::FormParameters;
 use app_state::AppState;
 use platform::{MyResponse, Platform, ContentType};
 use serde_json::Value;
-use std::env;
-use std::fs::File;
 use std::sync::Arc;
-use std::net::SocketAddr;
-use hyper::{header, Body, Request, Response, Server, Error, StatusCode, Method};
-use hyper::service::{make_service_fn, service_fn};
-//type GenericError = Box<dyn std::error::Error + Send + Sync>;
+use url::form_urlencoded;
+use std::fs::File;
 
+const MAX_POST_SIZE: u64 = 1024 * 1024 * 128; // MB
 static NOTFOUND: &[u8] = b"Not Found";
+static BODY_TOO_BIG: &[u8] = b"POST body too large";
 
 async fn process_form(parameters:&str, state: Arc<AppState>) -> MyResponse {
     let parameter_pairs = form_urlencoded::parse(parameters.as_bytes())
@@ -170,32 +177,30 @@ async fn process_form(parameters:&str, state: Arc<AppState>) -> MyResponse {
 }
 
 
-
 /// HTTP status code 404
-fn not_found() -> Result<Response<Body>,Error> {
+fn not_found() -> Result<Response<Full<Bytes>>,Infallible> {
     Ok(Response::builder()
         .status(StatusCode::NOT_FOUND)
         .body(NOTFOUND.into())
         .unwrap())
 }
 
-async fn simple_file_send(filename: &str,content_type: &str) -> Result<Response<Body>,Error> {
-    // Serve a file by asynchronously reading it by chunks using tokio-util crate.
+async fn simple_file_send(filename: &str,content_type: &str) -> Result<Response<Full<Bytes>>,Infallible> {
     let filename = format!("html{}",filename);
-    if let Ok(file) = TokioFile::open(filename).await {
-        let stream = FramedRead::new(file, BytesCodec::new());
-        let body = Body::wrap_stream(stream);
-        let response = Response::builder()
-        .header(header::CONTENT_TYPE, content_type)
-        .body(body)
-        .unwrap();
-        return Ok(response);
+    match std::fs::read(&filename) {
+        Ok(bytes) => {
+            let body = Full::from(bytes);
+            let response = Response::builder()
+                .header(header::CONTENT_TYPE, content_type)
+                .body(body)
+                .unwrap();
+            Ok(response)
+            },
+        Err(_) => not_found(),
     }
-
-    not_found()
 }
 
-async fn serve_file_path(filename:&str) -> Result<Response<Body>,Error> {
+async fn serve_file_path(filename:&str) -> Result<Response<Full<Bytes>>,Infallible> {
     match filename {
         "/" => simple_file_send("/index.html","text/html; charset=utf-8").await,
         "/index.html" => simple_file_send(filename,"text/html; charset=utf-8").await,
@@ -207,17 +212,19 @@ async fn serve_file_path(filename:&str) -> Result<Response<Body>,Error> {
     }
 }
 
-async fn process_from_query(query:&str,app_state:Arc<AppState>) -> Result<Response<Body>,Error> {
+async fn process_from_query(query:&str,app_state:Arc<AppState>) -> Result<Response<Full<Bytes>>,Infallible> {
     let ret = process_form(query,app_state).await;
     let response = Response::builder()
         .header(header::CONTENT_TYPE, ret.content_type.as_str())
         .header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-        .body(Body::from(ret.s))
+        .body(Full::from(ret.s))
         .unwrap();
     Ok(response)
-}
+} 
 
-async fn process_request(mut req: Request<Body>,app_state:Arc<AppState>) -> Result<Response<Body>,Error> {
+async fn process_request(req: Request<hyper::body::Incoming>,app_state:Arc<AppState>) -> Result<Response<Full<Bytes>>, Infallible> {
+    let path = req.uri().path().to_string();
+
     // URL GET query
     if let Some(query) = req.uri().query() {
         if !query.is_empty() {
@@ -227,7 +234,13 @@ async fn process_request(mut req: Request<Body>,app_state:Arc<AppState>) -> Resu
 
     // POST
     if req.method() == Method::POST {
-        let query = hyper::body::to_bytes(req.body_mut()).await.unwrap();
+        let upper = req.body().size_hint().upper().unwrap_or(u64::MAX);
+        if upper > MAX_POST_SIZE {
+            let mut resp = Response::new(Full::from(BODY_TOO_BIG));
+            *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
+            return Ok(resp);
+        }
+        let query = req.collect().await.unwrap().to_bytes();
         if !query.is_empty() {
             let query = String::from_utf8_lossy(&query);
             return process_from_query(&query,app_state).await;
@@ -235,8 +248,64 @@ async fn process_request(mut req: Request<Body>,app_state:Arc<AppState>) -> Resu
     }
 
     // Fallback: Static file
-    serve_file_path(req.uri().path()).await
+    serve_file_path(&path).await
+    
 }
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    tracing_subscriber::fmt::init();
+
+    let basedir = env::current_dir()
+        .expect("Can't get CWD")
+        .to_str()
+        .expect("Can't convert CWD to_str")
+        .to_string();
+    let path = basedir.to_owned() + "/config.json";
+    let file = File::open(&path).unwrap_or_else(|_| panic!("Can not open config file at {}", &path));
+    let petscan_config: Value = serde_json::from_reader(file).expect("Can not parse JSON from config file");
+
+    // Shared state
+    let app_state = Arc::new(AppState::new_from_config(&petscan_config).await) ;
+
+    // Run on IP/port
+    let port = petscan_config["http_port"].as_u64().unwrap_or(80) as u16;    
+    let ip_address = petscan_config["http_server"].as_str().unwrap_or("0.0.0.0").to_string();
+    let ip_address : Vec<u8> = ip_address.split('.').map(|s|s.parse::<u8>().unwrap()).collect();
+    let ip_address = std::net::Ipv4Addr::new(ip_address[0],ip_address[1],ip_address[2],ip_address[3],);
+    let addr = SocketAddr::from((ip_address, port));
+    println!("Listening on http://{}", addr);
+
+    // We create a TcpListener and bind it to IP:port
+    let listener = TcpListener::bind(addr).await?;
+
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+        let app_state_clone = app_state.clone();
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(  |req| {
+                    process_request(req,app_state_clone.clone())
+                }))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
+
+
+/*
 
 #[tokio::main]
 async fn main() -> Result<(),Error> {
@@ -278,3 +347,4 @@ async fn main() -> Result<(),Error> {
 
     Ok(())
 }
+ */
