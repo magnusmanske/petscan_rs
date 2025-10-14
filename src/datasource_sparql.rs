3acb59f3ce402f2af6f43292a2fd40da0966462b
Magnus Manske <magnusmanske@googlemail.com>
2025-10-14 15:31:10 +0300

    enabling QLever

--- src/datasource_sparql.rs
+++ src/datasource_sparql.rs
@@ -8,47 +8,60 @@ use std::collections::HashMap;
 use std::time;
 use wikimisc::mediawiki::api::Api;
 
-#[derive(Debug, Clone, PartialEq, Default, Copy)]
-pub struct SourceSparql;
+const QLEVER_WD_PREFIX: &str = "PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>";
 
-#[async_trait]
-impl DataSource for SourceSparql {
-    fn name(&self) -> String {
-        "sparql".to_string()
+enum SparqlServer {
+    QLeverWd,
+    Wikidata,
+}
+
+impl SparqlServer {
+    fn from(s: Option<String>) -> Self {
+        match s.as_deref() {
+            Some("qlever_wd") => SparqlServer::QLeverWd,
+            _ => SparqlServer::Wikidata,
+        }
     }
 
-    fn can_run(&self, platform: &Platform) -> bool {
-        platform.has_param("sparql")
+    const fn url(&self) -> &str {
+        match self {
+            SparqlServer::QLeverWd => "https://qlever.cs.uni-freiburg.de/api/wikidata",
+            SparqlServer::Wikidata => "https://query.wikidata.org/sparql",
+        }
     }
 
-    async fn run(&mut self, platform: &Platform) -> Result<PageList> {
-        let sparql = platform
-            .get_param("sparql")
-            .ok_or_else(|| anyhow!("Missing parameter \'sparql\'"))?;
+    fn add_prefix(&self, sparql: &str) -> String {
+        match self {
+            SparqlServer::QLeverWd => format!("{QLEVER_WD_PREFIX}\n{sparql}"),
+            SparqlServer::Wikidata => sparql.to_string(),
+        }
+    }
 
-        let timeout = time::Duration::from_secs(120);
-        let builder = reqwest::ClientBuilder::new().timeout(timeout);
-        let api = Api::new_from_builder("https://www.wikidata.org/w/api.php", builder).await?;
-
-        let sparql_url = api.get_site_info_string("general", "wikibase-sparql")?;
-        let mut params: HashMap<String, String> = HashMap::new();
-        params.insert("query".to_string(), sparql.to_string());
-        params.insert("format".to_string(), "json".to_string());
-
-        let response = match api
-            .client()
-            .post(sparql_url)
-            .header(reqwest::header::USER_AGENT, "PetScan")
-            .form(&params)
-            .send()
-            .await
-        {
-            Ok(resp) => resp,
-            Err(e) => return Err(anyhow!("SPARQL: {e}")),
-        };
-
+    fn parse_response(&self, response: &str, api: &Api) -> Result<PageList> {
+        match self {
+            SparqlServer::QLeverWd => Self::parse_response_qlever_wd(response, api),
+            SparqlServer::Wikidata => Self::parse_response_wikidata(response, api),
+        }
+    }
+    fn parse_response_qlever_wd(response: &str, api: &Api) -> Result<PageList> {
+        println!("Using QLever");
+        let result: Value = serde_json::from_str(response)?;
+        let first_var = result["head"]["vars"][0]
+            .as_str()
+            .ok_or(anyhow!("No variables found in SPARQL result"))?;
         let ret = PageList::new_from_wiki("wikidatawiki");
-        let response = response.text().await.map_err(|e| anyhow!(e))?;
+        api.entities_from_sparql_result(&result, first_var)
+            .iter()
+            .filter_map(|e| Platform::entry_from_entity(e))
+            .for_each(|entry| ret.add_entry(entry));
+        Ok(ret)
+    }
+
+    fn parse_response_wikidata(response: &str, api: &Api) -> Result<PageList> {
+        println!("Using WIkidata");
+        let ret = PageList::new_from_wiki("wikidatawiki");
         let mut mode: u8 = 0;
         let mut header = String::new();
         let mut binding = String::new();
@@ -90,36 +103,54 @@ impl DataSource for SourceSparql {
                 },
             }
         }
-
         Ok(ret)
     }
+}
 
-    /*
-    // using serde, obsolete because of high memory usage
-    fn run(&mut self, platform: &Platform) -> Result<PageList> {
-        let sparql = platform
+#[derive(Debug, Clone, PartialEq, Default, Copy)]
+pub struct SourceSparql;
+
+#[async_trait]
+impl DataSource for SourceSparql {
+    fn name(&self) -> String {
+        "sparql".to_string()
+    }
+
+    fn can_run(&self, platform: &Platform) -> bool {
+        platform.has_param("sparql")
+    }
+
+    async fn run(&mut self, platform: &Platform) -> Result<PageList> {
+        let sparql_param = platform
             .get_param("sparql")
-            .ok_or(format!("Missing parameter 'sparql'"))?;
+            .ok_or_else(|| anyhow!("Missing parameter \'sparql\'"))?;
 
-        let timeout = Some(time::Duration::from_secs(120));
-        let builder = reqwest::blocking::ClientBuilder::new().timeout(timeout);
-        let api = Api::new_from_builder("https://www.wikidata.org/w/api.php", builder)
-            .map_err(|e| format!("SourceSparql::run:1 {:?}", e))?;
-        let result = api
-            .sparql_query(sparql.as_str())
-            .map_err(|e| format!("SourceSparql::run:2 {:?}", e))?;
-        let first_var = result["head"]["vars"][0]
-            .as_str()
-            .ok_or(format!("No variables found in SPARQL result"))?;
-        let ret = PageList::new_from_wiki("wikidatawiki");
-        api.entities_from_sparql_result(&result, first_var)
-            .par_iter()
-            .filter_map(|e| Platform::entry_from_entity(e))
-            .for_each(|entry| ret.add_entry(entry));
-        if ret.is_empty() {
-            platform.warn(format!("<span tt='warn_sparql'></span>"));
-        }
-        Ok(ret)
+        let timeout = time::Duration::from_secs(120);
+        let builder = reqwest::ClientBuilder::new().timeout(timeout);
+        let api = Api::new_from_builder("https://www.wikidata.org/w/api.php", builder).await?;
+
+        // let sparql_url = api.get_site_info_string("general", "wikibase-sparql")?;
+        let sparql_server = SparqlServer::from(platform.get_param("sparql_server"));
+        let sparql_url = sparql_server.url();
+        let sparql = sparql_server.add_prefix(&sparql_param);
+
+        let mut params: HashMap<String, String> = HashMap::new();
+        params.insert("query".to_string(), sparql.to_string());
+        params.insert("format".to_string(), "json".to_string());
+
+        let response = match api
+            .client()
+            .post(sparql_url)
+            .header(reqwest::header::USER_AGENT, "PetScan")
+            .form(&params)
+            .send()
+            .await
+        {
+            Ok(resp) => resp,
+            Err(e) => return Err(anyhow!("SPARQL: {e}")),
+        };
+
+        let response = response.text().await.map_err(|e| anyhow!(e))?;
+        sparql_server.parse_response(&response, &api)
     }
-    */
 }
